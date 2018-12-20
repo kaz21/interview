82de1e9d1fc54d2e5d2f6378eebab7da7c101730
kyle zemek <kyle@wealthfront.com>
2018-12-20 10:20:59 -0800

    Default editors use

---
+++ src/main/java/Solution.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+
+public class Solution {
+
+  public static void main(String... args) {
+    JUnitCore.main("Solution");
+  }
+
+  @Test
+  public void test() { }
+
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+class SolutionTest {
+
+  @Test
+  void test() {}
+
+}
